1f1264907a25749fdba03eacc8f89b226ebd00c4
SafalSaxena <150792284+SafalSaxena@users.noreply.github.com>
2024-07-16 12:10:34 +0530

    Netflix-Clone

---
+++ src/firebase.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBL2rCj2R8lMfO1lyZGiJ1Eh5zo4v0SDEI",
+  authDomain: "netflix-clone2024.firebaseapp.com",
+  projectId: "netflix-clone2024",
+  storageBucket: "netflix-clone2024.appspot.com",
+  messagingSenderId: "543769218910",
+  appId: "1:543769218910:web:4b745e7354fa41205089cf",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { auth, db, login, signup, logout };
